0a4e281a601c2239ec67baf02c345d3dddd6bc32
Noah Scala <tobi8912@gmail.com>
2019-11-24 22:51:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -81,7 +81,7 @@ def monopoly_with_fourth_tier
         },
         :b_and_o_railroad=>
         {
-          "mortgage_value" => "$100"
+          "mortgage_value" => "$400"
         },
         :shortline_railroad=>
         {
